88d6ccf489779a954fad1f6afa92737c72fcfb78
juhyukm <juhyukm@gmail.com>
2023-12-19 00:46:27 +0900

    naming conventions

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,34 @@
+/**
+ * Naming Conventions
+ *
+ * 변수 이름 지을 때
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용할 수 있다.
+ * 2) 특기호는 언더스코어와 달러를 사용할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ *      1Name, 2Hello
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ *      var const = 'var';
+ */
+
+let codeFactory = '코드팩토리';
+var $world = '세계';
+const _juHyun = '현';
+
+console.log(codeFactory);
+console.log($world);
+console.log(_juHyun);
+
+// let 1name = 'no';
+// let const = 'attention';
+
+/**
+ * Naming Conventions 2
+ *
+ * 1) camelCase -> 대부분의 언어에서 많이 사용
+ * 2) snake_case -> 파이썬같은 언어에서 사용
+ * 3) PascalCase -> C# 마이크로소프트 계열의 언어에서 사용함
+ */
+
+// 김태희
+const kimTaeHee = '김태희';
+console.log(kimTaeHee);
